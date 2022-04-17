b88c762d39098633e22145bfb7d97b3c5881ce3d
Gabriel Lopes <gabrielrrlopes@gmail.com>
2022-04-17 19:36:25 -0300

    Changed Slider component in oder to behavior as a carousel.

--- src/Pages/Slider/index.tsx
+++ src/Pages/Slider/index.tsx
@@ -1,5 +1,21 @@
 import "./styles.css";
-import Ads from "../../Assets/sponsors_v2/one.jpg";
+
+import One from "../../Assets/sponsors_v2/one.jpg";
+import Two from "../../Assets/sponsors_v2/two.jpg";
+import Three from "../../Assets/sponsors_v2/three.jpg";
+import Four from "../../Assets/sponsors_v2/four.jpg";
+import Five from "../../Assets/sponsors_v2/five.jpg";
+import Six from "../../Assets/sponsors_v2/six.jpg";
+import Seven from "../../Assets/sponsors_v2/seven.jpg";
+import Eight from "../../Assets/sponsors_v2/eight.jpg";
+import Nine from "../../Assets/sponsors_v2/nine.jpg";
+import Ten from "../../Assets/sponsors_v2/ten.jpg";
+import Eleven from "../../Assets/sponsors_v2/eleven.jpg";
+import Twelve from "../../Assets/sponsors_v2/twelve.jpg";
+import Thirteen from "../../Assets/sponsors_v2/thirteen.jpg";
+
+import { Carousel } from "react-responsive-carousel";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 interface Props {
   className: string;
@@ -8,9 +24,56 @@ interface Props {
 const Slide = (props: Props) => {
   return (
     <div className={`${props.className} slider_flex`}>
-      <div>
-        <img src={Ads} />
-      </div>
+      <Carousel
+        autoPlay={true}
+        interval={1000}
+        infiniteLoop={true}
+        showThumbs={false}
+        showIndicators={false}
+        swipeable={false}
+        showStatus={false}
+      >
+        <div>
+          <img src={One} />
+        </div>
+        <div>
+          <img src={Two} />
+        </div>
+        <div>
+          <img src={Three} />
+        </div>
+        <div>
+          <img src={Four} />
+        </div>
+        <div>
+          <img src={Five} />
+        </div>
+        <div>
+          <img src={Six} />
+        </div>
+        <div>
+          <img src={Seven} />
+        </div>
+        <div>
+          <img src={Eight} />
+        </div>
+        <div>
+          <img src={Nine} />
+        </div>
+        <div>
+          <img src={Ten} />
+        </div>
+        <div>
+          <img src={Eleven} />
+        </div>
+        <div>
+          <img src={Twelve} />
+        </div>
+        <div>
+          <img src={Thirteen} />
+        </div>
+
+      </Carousel>
     </div>
 
   )
