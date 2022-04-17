fcb8915cde23a825c73d3f98c3f719a8f5cd873f
Gabriel Lopes <gabrielrrlopes@gmail.com>
2022-04-17 19:37:40 -0300

    Added Footer component

---
+++ src/Pages/Footer/index.tsx
@@ -0,0 +1,18 @@
+import "./styles.css"
+import Bottom from "../../Assets/bannerTop/home_v3.png"
+
+interface Props {
+  className: string;
+}
+
+const Footer = (props: Props) => {
+  return (
+    <div className={`${props.className} footer_container_flex`}>
+      <div className="footer_flex_item">
+        <img src={Bottom} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export { Footer }
