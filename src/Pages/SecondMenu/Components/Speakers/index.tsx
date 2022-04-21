94fb01b68fcc59df62f8c2ec23cb2388821d698c
Gabriel Lopes <gabrielrrlopes@gmail.com>
2022-04-21 14:41:19 -0300

    Speakers component was implemented.

---
+++ src/Pages/SecondMenu/Components/Speakers/index.tsx
@@ -0,0 +1,180 @@
+import "./styles.css"
+
+import Adam from "../../../../Assets/speakers_v2/adam-teolis-circle.jpg";
+
+interface Props {
+  className: string;
+}
+
+const Speakers = (props: Props) => {
+  return (
+    <div>
+      <div className="topheader">
+        <div>
+          <h1>Speakers</h1>
+        </div>
+      </div>
+      <div className={`${props.className} secondmenucontents_container_flex holdcontent`}>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+        <div className="secondmenucontents">
+          <div>
+            <img src={Adam} alt="" />
+          </div>
+          <div>
+            <h1>adam teolis</h1>
+          </div>
+          <div>
+            <p>founder & ceo, chefdrop</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export { Speakers }
